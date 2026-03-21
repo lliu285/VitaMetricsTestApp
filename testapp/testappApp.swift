14e2cf37d07d31809de65e5e3fe63d3853145570
Lucia Liu <lucia.liu14@gmail.com>
2026-03-21 16:25:49 -0700

    Initial Commit

---
+++ testapp/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  testapp
+//
+//  Created by Lucia Liu on 3/21/26.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
